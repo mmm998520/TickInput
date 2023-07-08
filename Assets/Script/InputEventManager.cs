a38e66bf8de8391ceb0d20a639f8289add1f16c1
mmm998520 <mmm998520@gmail.com>
2023-07-08 12:48:30 +0800

    建立通用物件池、建立數學優化、新增註解

---
+++ Assets/Script/InputEventManager.cs
@@ -0,0 +1,81 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace TickEventSystem
+{
+    /// <summary>
+    /// 輸入資訊管理
+    /// </summary>
+    public class InputEventManager : MonoBehaviour
+    {
+        /// <summary>
+        /// 須進行管理的InputActions
+        /// </summary>
+        public InputActions inputActions;
+        /// <summary>
+        /// 尚未處理的所有輸入事件
+        /// </summary>
+        public List<TickEvent> tickEvents = new List<TickEvent>();
+        /// <summary>
+        /// 輸入事件物件池
+        /// </summary>
+        public ObjPool<TickEvent> tickEventPool = new ObjPool<TickEvent>(30);
+
+        private void Awake()
+        {
+            //開始遊戲時建立輸入器
+            setInputManager();
+        }
+
+        void Start()
+        {
+            //紀錄遊戲開始時間為第0幀
+            TickEvent.setGameStartTime();
+            //開始遊戲時建立監聽器
+            creatEventListener();
+        }
+
+        /// <summary>
+        /// 確保有需管理的InputActions，並將它開啟
+        /// </summary>
+        void setInputManager()
+        {
+            if (inputActions == null)
+            {
+                inputActions = new InputActions();
+            }
+            inputActions.Enable();
+        }
+
+        /// <summary>
+        /// 開啟所有InputActions監聽
+        /// </summary>
+        void creatEventListener()
+        {
+            inputActions.InputMap_Keyboard.a.started += inputTickEvent;
+            inputActions.InputMap_Keyboard.a.performed += inputTickEvent;
+            inputActions.InputMap_Keyboard.a.canceled += inputTickEvent;
+        }
+
+        /// <summary>
+        /// 監聽回傳函式
+        /// </summary>
+        /// <param name="obj">input的監聽事件資訊</param>
+        private void inputTickEvent(InputAction.CallbackContext obj)
+        {
+            TickEvent tickEvent = tickEventPool.newObject();
+            tickEvent.setTickEvent(TickEvent.DataTimeToTick(), obj.phase.ToString());
+            tickEvents.Add(tickEvent);
+            Debug.LogWarning(tickEvent.eventContent + ", " + tickEvent.eventTriggerTick);
+        }
+        int i = 0;
+        void Update()
+        {
+            print(tickEventPool.pool.Count);
+        }
+    }
+}
---
+++ Assets/Script/ObjPool.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 通用物件池，實例化後可管理所有類型的class
+/// </summary>
+/// <typeparam name="T">必須為Class，且必需有new T()無參數建構子</typeparam>
+public class ObjPool<T> where T : class, new()
+{
+    public Stack<T> pool = new Stack<T>();
+
+    /// <summary>
+    /// 物件池實例化建構式
+    /// </summary>
+    public ObjPool()
+    {
+
+    }
+
+    /// <summary>
+    /// 物件池實例化建構式
+    /// </summary>
+    /// <param name="initialSize">建構後預先創建多少物件於物件池中</param>
+    public ObjPool(int initialSize)
+    {
+        for (int i = 0; i < initialSize; i++)
+        {
+            pool.Push(new T());
+        }
+    }
+
+    /// <summary>
+    /// 從物件池中抽取物件，若物件池數量不足，創造新物件
+    /// </summary>
+    /// <returns></returns>
+    public T newObject()
+    {
+        if (pool.Count > 0)
+        {
+            return pool.Pop();
+        }
+        else
+        {
+            return new T();
+        }
+    }
+
+    /// <summary>
+    /// 將用完的物件返回至物件池中
+    /// </summary>
+    /// <param name="obj"></param>
+    public void ReturnObject(T obj)
+    {
+        pool.Push(obj);
+    }
+}
